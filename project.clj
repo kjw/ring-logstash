7b0c14bbd13f46ae4b2cbd8c97f9548478fb02fc
Karl Jonathan Ward <karl.j.ward@gmail.com>
2014-03-03 11:12:02 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kjw/ring-logstash "0.1.1"
+(defproject kjw/ring-logstash "0.1.2"
   :description "Send ring requests, response statuses and exceptions to logtash via TCP."
   :url "http://github.com/kjw/ring-logstash"
   :license {:name "Eclipse Public License"
